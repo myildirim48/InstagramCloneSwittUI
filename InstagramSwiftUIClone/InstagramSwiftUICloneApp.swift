5f0b0468395c8b1844ebe677544cad254a3a10ea
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2023-05-03 20:35:33 +0300

    Initial Commit

---
+++ InstagramSwiftUIClone/InstagramSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUICloneApp.swift
+//  InstagramSwiftUIClone
+//
+//  Created by YILDIRIM on 3.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
